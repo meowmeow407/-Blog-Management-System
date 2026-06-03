30e5da4771a8cb840fbd69b4acf7382ace5a5752
Kshya <kshyam0722@gmail.com>
2026-06-03 19:33:03 +0530

    Patch database connection mapping for Render URL handling

--- config/database.php
+++ config/database.php
@@ -86,7 +86,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'), // <-- FIXED HERE to read Render's database URL string natively
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
@@ -110,8 +110,6 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
     ],
